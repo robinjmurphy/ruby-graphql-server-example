226137bd494872943de8dc0157ac211c364b0674
Robin Murphy <robin@robinmurphy.co.uk>
2016-11-05 22:46:35 +0000

    Add reference to GraphQL HTTP docs

--- app.rb
+++ app.rb
@@ -5,12 +5,26 @@ require 'json'
 
 require_relative './lib/schema'
 
-def current_time
-  Time.now.iso8601
-end
+# This server implements the suggested HTTP protocol
+# as described in the GraphQL docs:
+#   http://graphql.org/learn/serving-over-http/
 
 before do
   content_type :json
+end
+
+get '/graphql' do
+  query = params['query']
+  variables = params['variables']
+  result = Schema.execute(query, variables: variables)
+  result.to_json
+end
+
+post '/graphql' do
+  pass unless request.content_type =~ /application\/graphql/
+  query = request.body.read
+  result = Schema.execute(query)
+  result.to_json
 end
 
 post '/graphql' do
@@ -20,3 +34,4 @@ post '/graphql' do
   result = Schema.execute(query, variables: variables)
   result.to_json
 end
+
